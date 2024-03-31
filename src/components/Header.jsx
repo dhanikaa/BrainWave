ebf0a36267150488010fd09f3a6e098de1bc9d66
dhanikaa <dhanikaanupama2000@gmail.com>
2024-03-31 13:15:35 +0530

    Minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from 'scroll-lock';
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
@@ -13,13 +14,17 @@ const Header = () => {
   const toggleNavigation = () => {
     if(openNavigation) {
         setOpenNavigation(false);
+        enablePageScroll();
     }
     else{
         setOpenNavigation(true);
+        disablePageScroll();
     }
   }
 
   const handleClick = () => {
+    if(!openNavigation) return;
+    enablePageScroll();
     setOpenNavigation(false);
   }
 
---
+++ src/components/Section.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section = (
+    className, id, crosses, crossesOffset, customPaddings
+) => {
+  return (
+    <div>Section</div>
+  )
+}
+
+export default Section
